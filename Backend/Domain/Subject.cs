cc6f1c340e5fac1a4f0bd0522ffab429b406b319
Oleh Prostakov <prostakov.oleh@gmail.com>
2024-03-02 16:58:14 +0200

    Implement SubjectsController

--- Backend/Domain/Subject.cs
+++ Backend/Domain/Subject.cs
@@ -2,14 +2,15 @@
 
 public class Subject : Entity<int>
 {
-    public Subject() {}
-    
-    public Subject(string title)
+    public Subject() { }
+
+    public Subject(string title, bool hasExam)
     {
         Title = title;
+        HasExam = hasExam;
     }
 
     public virtual string Title { get; set; }
-    public virtual IEnumerable<Activity> Activities { get; set; }
+    public virtual IEnumerable<Activity> Activities { get; set; } = new List<Activity>();
     public virtual bool HasExam { get; set; }
 }
--- Backend/Web/Controllers/SubjectsController.cs
+++ Backend/Web/Controllers/SubjectsController.cs
@@ -1,8 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using backend.ApiContracts;
+using domain;
+using domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers;
 
 public class SubjectsController : ControllerBase
 {
-    
+    private readonly IApplicationDbContext _dbContext;
+
+    public SubjectsController(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<IActionResult> CreateSubject([FromBody] SubjectContract apiSubject, CancellationToken token)
+    {
+        var subject = new Subject(apiSubject.Title, apiSubject.IsExam);
+        if (await _dbContext.Subjects.AnyAsync(x => x.Title == subject.Title, token))
+            return BadRequest("Subject already exists");
+
+        await _dbContext.Subjects.AddAsync(subject, token);
+        await _dbContext.SaveChangesAsync(token);
+
+        return NoContent();
+    }
 }
