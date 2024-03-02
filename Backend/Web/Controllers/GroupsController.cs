a820a8424f3efa3c98f88fe000887077c1e6bcbc
turoniol <turoniol29@gmail.com>
2024-03-02 17:15:32 +0200

    All groups mock

--- Backend/Web/Controllers/GroupsController.cs
+++ Backend/Web/Controllers/GroupsController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers;
 
 [Route("api/groups")]
-public class GroupsController: ControllerBase
+public class GroupsController : ControllerBase
 {
-    
+    private readonly IApplicationDbContext _context;
+    private readonly IConfiguration _configuration;
+
+    public GroupsController(IApplicationDbContext context, IConfiguration configuration)
+    {
+        _context = context;
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetGroups(CancellationToken token)
+    {
+        // var groups = await _context.Groups.ToListAsync(token);
+        var groups = new List<domain.Group> {
+            new(0, "КП-01"),
+            new(1, "KП-02"),
+            new(2, "KП-03"),
+        };
+        return Ok(groups.Select(x => new ApiContracts.GroupContract(x)));
+    }
 }
